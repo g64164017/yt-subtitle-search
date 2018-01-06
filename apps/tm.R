0cebcb791356bc21d3bd4276f048311011c3db1a
NASRUL HAMID <nasrul_hamid@apps.ipb.ac.id>
2018-01-06 20:40:22 +0700

    update Matriks TF-IDF

--- apps/tm.R
+++ apps/tm.R
@@ -11,40 +11,19 @@ df$cleanText <- gsub("[0-9]+:[0-9]+:[0-9]+.[0-9]+","'" , df$subtitle ,ignore.cas
 df$cleanText[1]
 tw <- data.frame(doc_id=df$video_id, text=df$cleanText)
 k <- Corpus(DataframeSource(tw))
-tdm <- TermDocumentMatrix(k, control = list(
+
+## TF-IDF
+tdm_tfidf <- TermDocumentMatrix(k, control = list(
   removePunctuation = TRUE,
   stopwords = TRUE,
   tolower = TRUE,
   stemming = FALSE,
-  stripWhitespace = TRUE))
+  stripWhitespace = TRUE,
+  weighting = function(x)
+    weightTfIdf(x, normalize =
+                  FALSE)
+))
 
+inspect(tdm_tfidf[1:10,1:5])
 
-## TF
-tf <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
-wf <- data.frame(word=names(tf), freq=tf)
-plot(wf$freq, main="Grafik Kata", xlab="Kata", ylab="Frek",type="l", col="blue")
-
-
-## DF - IDF
-DF_t <- sort(rowSums(as.matrix(tdm)!=0), decreasing=TRUE)
-
-# lihat 10 term teratas dan terbawah
-head(DF_t,10)
-tail(DF_t,10)
-
-# hitung IDF_t
-N <- length(colSums(as.matrix(tdm)))	# ukuran korpus
-IDF_t <- log(N/DF_t)			        # nilai IDFt
-
-# lihat 10 term teratas dan terbawah
-head(IDF_t,10)
-tail(IDF_t,10)
-
-# buat grafik TF-IDF
-tfidf_data <- data.frame(word=names(tf*IDF_t), freq=tf*IDF_t)
-tfidf_data= tfidf_data[with(tfidf_data, order(-freq)), ]
-plot(tfidf_data$freq, main="Grafik Kata", xlab="Kata", ylab="TF-IDF",type="l", col="blue")
-
-## SAVING
-write.csv("tf.csv", x=tf) 
-write.csv("tfidf.csv", x=tfidf_data) 
+write.csv("tfidf.csv", x=as.matrix(tdm_tfidf)) 
