2a2bcf37899df5175b5e4c922a40d76ba7ae6b15
NASRUL HAMID <nasrul_hamid@apps.ipb.ac.id>
2018-01-15 08:42:59 +0700

    searching handler function + apiserver using plumber

--- apps/query-1.R
+++ apps/query-1.R
@@ -18,32 +18,38 @@ library(tm)
 
 ## QUERYING
 
-q1 = "machine machine learning"
-q2 = "data mining"
-q = "principal component analysis"
+#* @get /search
+search_result = function(q = ""){
+  # q1 = "machine machine learning"
+  # q2 = "data mining"
+  # q = "principal component analysis"
+  
+  # # Query Weighting
+  newQry <- data.frame(doc_id="query", text = q)
+  newQryC <- Corpus(DataframeSource(newQry))
+  tdmNewQry <- TermDocumentMatrix(newQryC, control = list(
+    removePunctuation = TRUE,
+    stopwords = TRUE,
+    tolower = TRUE,
+    stemming = FALSE,
+    stripWhitespace = TRUE
+    # ,  weighting = weightTf
+    # ,dictionary=dictC
+  ))
+  tdmNewQry = as.matrix(tdmNewQry)
+  
+  tq_idf = tdmNewQry * IDF_t$V1[IDF_t$X %in% rownames(tdmNewQry)]
+  
+  len_d = sqrt(col_sums(tfidf_data[,-1]^2))
+  len_q = sqrt(col_sums(tq_idf^2))
+  t = data.frame(tfidf_data[tfidf_data$X %in% rownames(tdmNewQry),-1])
+  prod_dq = col_sums(t * as.matrix(tdmNewQry))
+  names(prod_dq) = names(t)
+  res = prod_dq/(len_d*len_q)
+  rank = sort(res[res!=0.0],decreasing = TRUE)
+  n = length(rank)
+  
+  return(ds_seq[gsub("X","",names(rank[1:n])),c("docid","video_id","time_start","url")])
+}
 
-# # Query Weighting
-newQry <- data.frame(doc_id="query", text = q)
-newQryC <- Corpus(DataframeSource(newQry))
-tdmNewQry <- TermDocumentMatrix(newQryC, control = list(
-  removePunctuation = TRUE,
-  stopwords = TRUE,
-  tolower = TRUE,
-  stemming = FALSE,
-  stripWhitespace = TRUE
-  # ,  weighting = weightTf
-  # ,dictionary=dictC
-))
-tdmNewQry = as.matrix(tdmNewQry)
-
-tq_idf = tdmNewQry * IDF_t$V1[IDF_t$X %in% rownames(tdmNewQry)]
-
-len_d = sqrt(col_sums(tfidf_data[,-1]^2))
-len_q = sqrt(col_sums(tq_idf^2))
-t = data.frame(tfidf_data[tfidf_data$X %in% rownames(tdmNewQry),-1])
-prod_dq = col_sums(t * as.matrix(tdmNewQry))
-names(prod_dq) = names(t)
-res = prod_dq/(len_d*len_q)
-rank = sort(res[res!=0.0],decreasing = TRUE)
-n = length(rank)
-ds_seq[gsub("X","",names(rank[1:n])),c("docid","video_id","time_start","url")]
+search_result(q = "information retrieval")
